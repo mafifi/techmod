652a8761c0ef8458d804b383f0c45aeebbfde7b0
mafifi <m.afifi@gmail.com>
2025-08-27 14:10:35 +0100

    feat: initial commit

---
+++ src/app.d.ts
@@ -0,0 +1,17 @@
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+	namespace App {
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface PageState {}
+		interface Platform {
+			Env: {
+				PUBLIC_CONVEX_URL: string;
+			};
+		}
+	}
+}
+
+export {};
