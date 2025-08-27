ee0b4edf437438fe624c2dd344c67cd7f08418ad
mafifi <m.afifi@gmail.com>
2025-08-27 14:26:58 +0100

    fix: update import for dynamic environment variables in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { PUBLIC_CONVEX_URL } from '$env/static/public';
+	import { env } from '$env/dynamic/public';
 	import { setupConvex } from 'convex-svelte';
 
 	let { children } = $props();
 
-	setupConvex(PUBLIC_CONVEX_URL );
+	setupConvex(env.PUBLIC_CONVEX_URL);
 </script>
 
 <svelte:head>
