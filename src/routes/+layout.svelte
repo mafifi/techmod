c77f3e5bd0d995f49d61aeeb7cf2e7fde9eb7a5d
mafifi <m.afifi@gmail.com>
2025-09-04 11:16:40 +0100

    fix: update children rendering in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import { Toaster } from '$lib/ui/components/sonner';
 	import { TooltipProvider } from '$lib/ui/components/tooltip';
 
+	let { children } = $props();
 	setupConvex(env.PUBLIC_CONVEX_URL || '');
 </script>
 
@@ -14,6 +15,6 @@
 </svelte:head>
 
 <TooltipProvider delayDuration={0}>
-	<slot />
+	{@render children()}
 	<Toaster />
 </TooltipProvider>
